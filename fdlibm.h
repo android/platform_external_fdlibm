68272f6f0feb0311b8b6723c5564a3d5cb7a1853
Chih-Hung Hsieh <chh@google.com>
2016-06-10 11:14:25 -0700

    DO NOT SUBMIT, fix clang-tidy warnings in fdlibm
    Change-Id: Id427a73b43a36e5975dae549f4f508685c06eb20

--- fdlibm.h
+++ fdlibm.h
@@ -25,15 +25,15 @@ extern "C" {
 #endif
 
 #ifdef __LITTLE_ENDIAN
-#define __HI(x) *(1+(int*)&x)
+#define __HI(x) *(1+(int*)&(x))
 #define __LO(x) *(int*)&x
-#define __HIp(x) *(1+(int*)x)
+#define __HIp(x) *(1+(int*)(x))
 #define __LOp(x) *(int*)x
 #else
 #define __HI(x) *(int*)&x
-#define __LO(x) *(1+(int*)&x)
+#define __LO(x) *(1+(int*)&(x))
 #define __HIp(x) *(int*)x
-#define __LOp(x) *(1+(int*)x)
+#define __LOp(x) *(1+(int*)(x))
 #endif
 
 #ifndef __P
